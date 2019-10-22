01727896e6613851025dda7d11ec3396135c06a9
alkimides <inaki.seim@gmail.com>
2019-10-22 08:46:23 +0200

    Mi primer cambio

--- WebApplicationGitHub1/Startup.cs
+++ WebApplicationGitHub1/Startup.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Builder
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,6 +11,7 @@ namespace WebApplicationGitHub1
 {
     public class Startup
     {
+        //prueba1
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
